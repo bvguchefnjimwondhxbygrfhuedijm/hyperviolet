584e6e2cce401c2ba5420fffa2a7c18c78eaa431
bvguchefnjimwondhxbygrfhuedijm <143262150+bvguchefnjimwondhxbygrfhuedijm@users.noreply.github.com>
2023-10-02 10:27:52 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-  prefix: "/hyperviolet/uv/service/",
+  prefix: "/hyperviolet-us-server/uv/service/",
   bare: "https://8vk2d6-6987.csb.app/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
